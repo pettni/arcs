870686ab3af7529041bdfd4f987b014ad029fe3f
Petter Nilsson <pettni@umich.edu>
2017-02-04 12:52:21 -0500

    replaced function in communications toolbox

--- abstr-ref/@Rec/getVertexI.m
+++ abstr-ref/@Rec/getVertexI.m
@@ -13,7 +13,7 @@ function vert = getVertexI(rec, i)
 
   vert = rec.xmin;
   
-  act_fulldim = find(de2bi(i-1, n_fulldim));
+  act_fulldim = dec2bin(i-1, n_fulldim) - '0';
   vert(ind_fulldim(act_fulldim)) = rec.xmax(ind_fulldim(act_fulldim));
   
 end
